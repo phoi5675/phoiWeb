4260aaf9991f2ce1de5d729e0705a0d98407ce33
phoi5675 <phoi5675@gmail.com>
2022-02-19 19:14:53 +0900

    update about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./About.module.css";
+import { useEffect } from "react";
 
 const About = () => {
+  const img1 = require('../assets/img_1.jpeg');
+  const img2 = require('../assets/img_2.jpeg');
+  const [displayed, setDisplayed] = useState(img1);
+  const [toggle, setToggle] = useState(true);
+
+  useEffect(() => {
+    displayed === img1 ? setDisplayed(img2) : setDisplayed(img1);
+  }, [toggle]);
+
   return (
     <div className={styles.content}>
-      <h2>phoi5675@gmail.com</h2><br/>
-      <a href="https://github.com/phoi5675">Github</a>
+      <h1>About phoi</h1>
+      <p>강민재</p>
+      <p>phoi5675@gmail.com</p>
+      <a href="https://github.com/phoi5675"
+        className={styles.link}>Github</a>
+      <br/>
+      <img
+        src={displayed}
+        onClick={() => setToggle(!toggle)}
+        alt="myPic"/>
     </div>
   );
 };
