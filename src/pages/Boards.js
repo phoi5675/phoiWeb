e0359665049903ffa7a150f0c792a3b0570af832
phoi5675 <phoi5675@gmail.com>
2022-03-05 23:05:39 +0900

    add loading component when loading boardList

--- src/pages/Boards.js
+++ src/pages/Boards.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from './Boards.module.css';
+import styles from "./Boards.module.css";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
@@ -7,58 +7,62 @@ import { Link } from "react-router-dom";
 const Boards = () => {
   const [articles, setArticles] = useState([]);
   const { board } = useParams();
+  const loadingText = "loading...";
 
   useEffect(() => {
     fetch(`/webpage/boards/${board}/articles`)
-      .then(response => {
+      .then((response) => {
         return response.text();
       })
-      .then(respText => {
+      .then((respText) => {
         const json = JSON.parse(respText);
 
-        if (Object.keys(json).length > 0){
+        if (Object.keys(json).length > 0) {
           const _articles = json.map((article) => {
-              return <Article key={article.pk} article={article}/>
+            return <Article key={article.pk} article={article} />;
           });
           setArticles(_articles);
-        }
-        else {
-           setArticles([<p key={0}>NO ARTICLES!</p>]);
+        } else {
+          setArticles([<p key={0}>NO ARTICLES!</p>]);
         }
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   }, [board]);
 
   return (
-  <div className={styles.container}>
-    <p className={styles.header}>{board}</p>
-    <div>
-      {articles.map(article => article)}
+    <div className={styles.container}>
+      <p className={styles.header}>{board}</p>
+      <div>
+        {articles.length === 0
+          ? loadingText
+          : articles.map((article) => article)}
+      </div>
     </div>
-  </div>);
-}
+  );
+};
 
 export default Boards;
 
-const Article = ({article}) => {
+const Article = ({ article }) => {
   const formatDate = (dateStr) => {
-    const date = new Date(dateStr.replace('T', ' '));
+    const date = new Date(dateStr.replace("T", " "));
     return (
       `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()} ` +
       `${date.getHours()}:${date.getMinutes()}`
     );
-  }
+  };
   return (
     <div className={styles.article_container}>
-      <Link 
+      <Link
         className={styles.article_link}
         to={`/${article.fields.board_name}/${article.fields.title}`}
         board={article.fields.board_name}
-        title={article.fields.title}>
+        title={article.fields.title}
+      >
         <p className={styles.article_title}>{article.fields.title}</p>
         <p className={styles.article_date}>{formatDate(article.fields.date)}</p>
         <p className={styles.article_preview}>{article.fields.preview_text}</p>
       </Link>
     </div>
   );
-}
+};
